cd956269c49b83dd238a994ffd63ceaf4cac72d1
Fistashka <pavel.fistashka@gmail.com>
2022-06-23 00:48:10 +0300

    Added second functional customization button

--- Assets/Scripts/ActivateItems.cs
+++ Assets/Scripts/ActivateItems.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
 
 
@@ -9,6 +10,7 @@ public class ActivateItems : MonoBehaviour
     public SelectableMatrix selectableMatrix;
     public GameObject firstAnimation;
     private string selectMatrixPath;
+    public GameObject SecondItem;
     public void Start()
     {
         selectMatrixPath = $"{Application.persistentDataPath}/selectMatrix.json";
@@ -29,6 +31,15 @@ public class ActivateItems : MonoBehaviour
         else
         {
             firstAnimation.SetActive(false);
+        }
+
+        if(PlayerPrefs.GetInt("SecondItemSelected") == 1)
+        {
+            SecondItem.SetActive(true);
+        }
+        else
+        {
+            SecondItem.SetActive(false);
         }
     }
 
--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -13,15 +13,19 @@ public class ShopManager : MonoBehaviour
     public TMP_Text FirstButtonPrice;
     public Sprite lockedImage, unlockedImage;
     public Button firstButtonCheck; // BUTTONS!
-    public Image firstButtonStatusIcon;
-    public Image selectedStatusIcon;
+    public Image firstButtonStatusIcon, secondButtonStatusIcon;
+    public Image selectedStatusIcon, selectedStatusIcon2;
     public Sprite selected, notSelected;
     public int priceOfFirstItem;
+    public int priceOffSecondItem;
 
     private string unlockMatrixPath, selectMatrixPath;
 
     void Start()
     {
+        Debug.Log(PlayerPrefs.GetInt("SecondItem"));
+
+
         //PlayerPrefs.SetInt("SpentCoins", 0);//---------------------------------------RESET SPENT COINS
         FirstButtonPrice.text = priceOfFirstItem.ToString() + " Coins"; 
         //PlayerPrefs.SetInt("Coins", 100); //---------------------------------------MY COINTS CURRENTLY
@@ -80,7 +84,37 @@ public class ShopManager : MonoBehaviour
 
     }
 
-    
+    public void buySecondButton()
+    {
+        if (PlayerPrefs.GetInt("SecondItem") == 0)
+        {
+            if (PlayerPrefs.GetInt("Coins") >= priceOffSecondItem)
+            {
+                PlayerPrefs.SetInt("SecondItem", 1);
+                PlayerPrefs.SetInt("SpentCoins", PlayerPrefs.GetInt("SpentCoins") + priceOffSecondItem);
+            }
+
+        }
+        if (PlayerPrefs.GetInt("SecondItem") == 1)
+        {
+            if (PlayerPrefs.GetInt("SecondItemSelected") == 1)
+            {
+                selectedStatusIcon2.sprite = notSelected;
+                PlayerPrefs.SetInt("SecondItemSelected", 0);
+            }
+            else if (PlayerPrefs.GetInt("SecondItemSelected") == 0)
+            {
+                selectedStatusIcon2.sprite = selected;
+                PlayerPrefs.SetInt("SecondItemSelected", 1);
+            }
+        }
+
+    }
+
+
+
+
+
     public void RerenderShop()
     {
         if(unlockableMatrix.hasFirstButton)
@@ -99,6 +133,24 @@ public class ShopManager : MonoBehaviour
         else
         {
             selectedStatusIcon.sprite = notSelected;
+        }
+        //---------------------------------------------------------2(remove) - make array
+        if (PlayerPrefs.GetInt("SecondItem") == 1)
+        {
+            secondButtonStatusIcon.sprite = unlockedImage;
+        }
+        else
+        {
+            secondButtonStatusIcon.sprite = lockedImage;
+        }
+
+        if (PlayerPrefs.GetInt("SecondItemSelected") == 1)
+        {
+            selectedStatusIcon2.sprite = selected;
+        }
+        else
+        {
+            selectedStatusIcon2.sprite = notSelected;
         }
     }
 
